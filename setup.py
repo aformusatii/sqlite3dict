9048a9eae674dfc8c70be74c7631c36b6ea7c86b
bender <aformusatii@gmail.com>
2020-02-26 14:47:33 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/aformusatii/sqlite3dict",
     packages=setuptools.find_packages(),
+    install_requires=[
+      "sqlite3"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
